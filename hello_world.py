24687b95d65be3a43d96039cac4ea78b6863d0f7
Joe Stump <joe@stu.mp>
2024-11-12 09:52:14 -0500

    Add example Python plugin

---
+++ hello_world.py
@@ -0,0 +1,30 @@
+from flask_babel import gettext
+from searx.plugins import logger
+
+
+name = "Hello World"
+description = gettext("Test plugin that prints debug log messages")
+default_on = True
+preference_section = 'general'
+plugin_id = 'hello-world'
+
+
+logger = logger.getChild(plugin_id)
+
+
+def pre_search(request, search)
+    logger.info('Search: ', search)
+    logger.info('Request: ', request)
+    return True
+
+
+def post_search(request, search)
+    logger.info('Search: ', search)
+    logger.info('Request: ', request)
+    return None
+
+
+def on_result(request, search, result):
+    logger.info('Search: ', search)
+    logger.info('Result: ', result)
+    return True
---
+++ setup.py
@@ -0,0 +1,35 @@
+"""
+Searx privacy plugins
+
+searx-privacy-plugins
+  Plugins to handle redirecting URLs to private alternatives
+
+"""
+
+from setuptools import setup
+
+GIT_URL='https://gitea.stump.wtf/joestump/searxng-privacy-plugins'
+
+setup(
+    name                = 'searxng-privacy-plugins'
+    , version           = '0.1'
+    , description       = 'Privacy redirecting/proxying plugins'
+    , long_description  = __doc__
+    , url               =  GIT_URL
+    , author            = 'Joe Stump'
+    , author_email      = 'joe@stu.mp'
+    , project_urls      = {
+        "Code"              : GIT_URL
+        , "Issue tracker"   : GIT_URL + "/issues"
+    }
+    , license           = 'GNU Affero General Public License'
+    , zip_safe          = False
+    , py_modules        = [
+        'hello_world'
+    ]
+    , entry_points      = {
+        'searxng.plugins' : [
+            'searxng-privacy.green-results = hello_world'
+        ]
+    }
+)
